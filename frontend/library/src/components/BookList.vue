435d1edaa3018f616a059b24796e1e9478d55c79
np-223996 <np-223996@rwu.de>
2023-06-18 16:25:03 +0200

    Bilder über Google Api ISBN nummer ausgebessert

--- frontend/library/src/components/BookList.vue
+++ frontend/library/src/components/BookList.vue
@@ -23,7 +23,7 @@ export default {
   components: {
     Book
   },
-  data: function() {
+  data: function () {
     return {
       anzeigen: false,
       books: [],
@@ -31,85 +31,107 @@ export default {
     };
   },
   methods: {
-    borrow: function(book) {
+    borrow: function (book) {
       axios
-        .put("http://localhost:8080/books/"+book.id,{
+        .put("http://localhost:8080/books/" + book.id, {
           firstName: book.firstName,
           lastName: book.lastName,
           available: false
-          
+
         })
         .then(response => {
           this.books = response.data;
         });
     },
-    handback: function(book) {
+    handback: function (book) {
       axios
-        .put("http://localhost:8080/books/"+book.id,{
+        .put("http://localhost:8080/books/" + book.id, {
           firstName: '',
           lastName: '',
           available: true
-          
+
         })
         .then(response => {
           this.books = response.data;
         });
     },
-      async handleInput() {
+    async handleInput() {
       try {
         const response = await axios.get('http://localhost:8080/books/search/', {
-          params: { searchText:this.searchInput }
+          params: { searchText: this.searchInput }
         });
         this.books = response.data;
         this.getImgUrl();
       } catch (error) {
         console.error(error);
-      }    },
-    getImgUrl(){
-      for (let i = 0; i < this.books.length; i++){
-        this.books[i].img='https://covers.openlibrary.org/b/isbn/' + this.books[i].isbn + '-L.jpg'
       }
+    },
+    getGoogleImg() {
+      let imgresp
+      this.books.forEach(element => {
+        axios.get('https://www.googleapis.com/books/v1/volumes', {
+          params: {
+            q: 'isbn:' + element.isbn,
+            key: 'AIzaSyA8Dvs5T5N5GphcuRKPx2ilCcRJWSIvU1A'
+          }
+        }).then(response => {
+          console.log(element.isbn)
+          console.log(response)
+          try {
+            imgresp = response.data.items[0].volumeInfo.imageLinks.thumbnail
+            element.img = imgresp
+            console.log(imgresp)
+          } catch {
+            console.log('Error')
+          }
+        })
+
+      });
+
     }
 
   },
   mounted() {
     axios.get("http://localhost:8080/books/").then(response => {
       this.books = response.data;
-      this.getImgUrl()
+      this.getGoogleImg()
     });
   }
 };
 </script>
 
 <style>
-  #BookList{
-    background-color: bisque;
-    white-space: pre;
-  }
-  .Books{
-    margin-top: 20px;
-    margin-bottom: 20px;
+#BookList {
+  background-color: bisque;
+  white-space: pre;
+}
 
-  }
-  #borrow{
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    width: 70vw;
-    height: 5vh;
-    padding-bottom: 1vh;
-    margin-left: 3vw;
-    padding-left: 1vw;
-    justify-content: left;
-    border-bottom: black 1px solid;
-  }
-  #SearchBar{
-    background-color: grey;
-    margin-right: 50vw;
+.Books {
+  margin-top: 20px;
+  margin-bottom: 20px;
 
-  }
-  #SearchField{
-    background-color: aqua;
-    width: 50vw;
-    
-  }
-</style>
+}
+
+#borrow {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  width: 70vw;
+  height: 5vh;
+  padding-bottom: 1vh;
+  margin-left: 3vw;
+  padding-left: 1vw;
+  justify-content: left;
+  border-bottom: black 1px solid;
+}
+
+#SearchBar {
+  background-color: grey;
+  margin-right: 50vw;
+
+}
+
+#SearchField {
+  background-color: aqua;
+  width: 50vw;
+
+}</style>
