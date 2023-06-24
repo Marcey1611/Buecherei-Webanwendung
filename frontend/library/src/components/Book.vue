b4a1c27b51f358589676383582ec5c060dcc0f77
Marcey1611 <marcel.eichelberger@t-online.de>
2023-06-24 15:34:32 +0200

    CSS von Book BookLIst ausgelagert borrowCount bei BookList hinzugefügt

--- frontend/library/src/components/Book.vue
+++ frontend/library/src/components/Book.vue
@@ -1,6 +1,8 @@
-
 <template>
+  <v-chip class="borrowCountChip" v-tooltip="{ content: text }" @mouseover="showFullText = true" @mouseout="showFullText = false">
+    {{showFullText ? text : truncatedText }}</v-chip>
   <div id="book">
+    
     <div id="bookCoverspace">
       <img id="bookCover" :src="book.img" alt="" style="max-width: 100%;max-height: 100%;">
     </div>
@@ -27,52 +29,50 @@
   <v-expand-transition>
     <div v-if="showMoreInfos" id="bookMoreInfos">
       <div id="bookMoreInfosYearPagesLang">
-        <h3>Das Buch wurde {{ book.releaseYear }} mit {{ book.pages }} Seiten auf 
+        <h3>Das Buch wurde {{ book.releaseYear }} mit {{ book.pages }} Seiten auf
           {{ book.language }} veröffentlicht.</h3>
       </div>
       <h3 id="bookSumTitle">Zusammenfassung: </h3>
       <div id="bookSummary">{{ book.description }}</div>
-      <v-text-field class="bookListBorrowInputFirstName" v-if="book.available" 
-        type="text" placeholder="Vorname"
+      <v-text-field class="bookListBorrowInputFirstName" v-if="book.available" type="text" placeholder="Vorname"
         v-model="book.firstName"></v-text-field>
-      <v-text-field class="bookListBorrowInputLastName" v-if="book.available" 
-        type="text" placeholder="Nachname"
+      <v-text-field class="bookListBorrowInputLastName" v-if="book.available" type="text" placeholder="Nachname"
         v-model="book.lastName"></v-text-field>
       <v-btn id="bookListBorrowButton" v-if="book.available" @click="validateBorrowData">
         Ausleihen</v-btn>
-      <v-overlay id="overlayBorrowOverlay" v-model="overlayBorrowError" contained 
-        class="align-center justify-center">
+      <v-overlay id="overlayBorrowOverlay" v-model="overlayBorrowError" contained class="align-center justify-center">
         <div>
-<!--$4&AaLk#3@jG6y]xW~2mBd[0Cn^vZp(l1N`9O)bTfV}I?XrU{5]!Qq7eY+K;:S]8Dh,c.Mi<Ft*Rg>zHsE/-->
+          <!--$4&AaLk#3@jG6y]xW~2mBd[0Cn^vZp(l1N`9O)bTfV}I?XrU{5]!Qq7eY+K;:S]8Dh,c.Mi<Ft*Rg>zHsE/-->
           <v-icon class="overlayBorrowClose" @click="overlayBorrowError = false">mdi-close
-            </v-icon>
+          </v-icon>
           <div class="overlayBorrowText">
-            <h3 id="overlayBorrorErrorMessage">Bitte gib eine gültigen Vorname und 
+            <h3 id="overlayBorrorErrorMessage">Bitte gib eine gültigen Vorname und
               Nachnamen ein!</h3>
           </div>
         </div>
       </v-overlay>
-      <v-overlay id="overlayBorrowOverlay" v-model="overlayBorrow" contained 
-        class="align-center justify-center">
+      <v-overlay id="overlayBorrowOverlay" v-model="overlayBorrow" contained class="align-center justify-center">
         <div>
           <v-icon class="overlayBorrowClose" @click="overlayBorrow = false">mdi-close
-            </v-icon>
+          </v-icon>
           <div class="overlayBorrowText">
             <h3>Weiterse Vorgehen:</h3>
             <br>
             <h4>Herzlichen Glückwunsch!</h4>
             <p>Du hast soeben das Buch {{ book.title }} von {{ book.author }} ausgeliehen.</p>
-            <p>Jetzt kannst du dich bei seinem Besitzer {{ book.owner.firstName }} {{ book.owner.lastName }} unter den
-              folgenden Kontaktdaten melden.
+            <p>Jetzt kannst du dich bei seinem Besitzer {{ book.owner.firstName }}
+              {{ book.owner.lastName }} unter den folgenden Kontaktdaten melden.
             </p>
             <br>
             <p>Kontaktdaten:</p>
             <p>Handynummer des Besitzers: {{ book.owner.phoneNumber }}</p>
             <p>E-Mail des Besitzers: {{ book.owner.eMail }}</p>
             <br>
-            <h4>Um den Vorgang abzuschließen und um zu bestätigen das du das Buch sicher ausleihen möchtest,</h4>
+            <h4>Um den Vorgang abzuschließen und um zu bestätigen das du das Buch sicher
+              ausleihen möchtest,</h4>
             <h4>klicke auf den Button unten, du gelangst so zur Bücherliste zurück</h4>
-            <br><v-btn id="overlayBorrowButton" @click="borrow(book)">Bestätigen</v-btn>
+            <br>
+            <v-btn id="overlayBorrowButton" @click="borrow(book)">Bestätigen</v-btn>
           </div>
         </div>
       </v-overlay>
@@ -90,7 +90,10 @@ export default {
       show: false,
       overlayBorrow: false,
       overlayBorrowError: false,
-      showMoreInfos: false
+      showMoreInfos: false,
+      text: '9x ausgeliehen',
+      truncatedText: '9x',
+      showFullText: false
     };
   },
   props: ['book'],
@@ -110,182 +113,10 @@ export default {
       }
 
     }
-  }
+  },
 };
 </script>
 
 <style>
-#book {
-  background-color: #2d2d2d;
-  color: #828282;
-  padding-left: 1vw;
-  padding-right: 1vw;
-  padding-top: 1vh;
-  padding-bottom: 3vh;
-  display: grid;
-  grid-template-columns: 2fr 8fr 1fr;
-  grid-template-rows: 8vh 6vh 6vh;
-  height: max-content;
-  box-shadow: 4px 4px 8px rgba(0, 0, 0, 0.2);
-  border-radius: 2vh 2vh 0vh 0vh;
-  border-bottom: #828282 1px solid;
-  width: 100%;
-}
-#bookCoverspace {
-  grid-column-start: 1;
-  grid-column-end: 2;
-  grid-row-start: 1;
-  grid-row-end: 4;
-  width: 15vh;
-  height: 20vh;
-}
-#bookTitle {
-  grid-column-start: 2;
-  grid-column-end: 4;
-  grid-row-start: 1;
-  grid-row-end: 2;
-}
-#bookOwnerIcon {
-  float: right;
-  background-color: #0E639C;
-  color: white;
-  height: 5vh;
-  width: 5vw;
-}
-#bookAuthor {
-  grid-column-start: 2;
-  grid-column-end: 4;
-  grid-row-start: 2;
-  grid-row-end: 3;
-}
-#bookGenre {
-  grid-column-start: 2;
-  grid-column-end: 4;
-  grid-row-start: 3;
-  grid-row-end: 4;
-}
-#moreInfosChevronButton {
-  grid-column-start: 4;
-  grid-column-end: 5;
-  grid-row-start: 3;
-  grid-row-end: 4;
-  text-align: center;
-  font-size: 3vh;
-  color: #828282;
-}
-#moreInfosChevronButton:hover {
-  color: white;
-}
-#moreInfosChevronButton:active {
-  color: #0E639C;
-}
-#bookMoreInfos {
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  grid-template-rows: 5vh 5vh 10vh 10vh;
-  width: 100%;
-  color: #828282;
-  background-color: #2d2d2d;
-  padding-left: 1vw;
-  padding-right: 1vw;
-  padding-top: 3vh;
-  padding-bottom: 1vh;
-  border-radius: 0vh 0vh 1vh 1vh;
-}
-#bookMoreInfosYearPagesLang {
-  grid-column-start: 1;
-  grid-column-end: 4;
-  grid-row-start: 1;
-  grid-row-end: 2;
-}
-#bookSumTitle {
-  grid-column-start: 1;
-  grid-column-end: 4;
-  grid-row-start: 2;
-  grid-row-end: 3;
-  margin-bottom: 10px;
-}
-#bookSummary {
-  grid-column-start: 1;
-  grid-column-end: 4;
-  grid-row-start: 3;
-  grid-row-end: 4;
-  white-space: pre-wrap;
-  overflow-y: auto;
-}
-.bookListBorrowInputFirstName {
-  grid-column-start: 1;
-  grid-column-end: 2;
-  grid-row-start: 4;
-  grid-row-end: 5;
-  padding-right: 2vw;
-  padding-top: 3vh;
-  height: 6vh;
-  color: white;
-}
-.bookListBorrowInputLastName {
-  grid-column-start: 2;
-  grid-column-end: 3;
-  grid-row-start: 4;
-  grid-row-end: 5;
-  padding-right: 2vw;
-  padding-top: 3vh;
-  height: 6vh;
-  color: white;
-}
-#bookListBorrowButton {
-  grid-column-start: 3;
-  grid-column-end: 4;
-  grid-row-start: 4;
-  grid-row-end: 5;
-  margin-top: 3vh;
-  height: 6vh;
-  background-color: #0E639C;
-  color: white;
-}
-#overlayBorrowOverlay {
-  position: fixed;
-}
-.overlayBorrowText {
-  text-align: center;
-  background-color: #2d2d2d;
-  color: #828282;
-  width: max-content;
-  height: max-content;
-  padding-top: 1vh;
-  padding-bottom: 1vh;
-  padding-left: 5vw;
-  padding-right: 5vw;
-  border-radius: 1vh 1vh 1vh 1vh;
-  box-shadow: 4px 4px 8px rgba(0, 0, 0, 0.2);
-}
-#overlayBorrorErrorMessage {
-  color: #C0321E;
-}
-.overlayBorrowClose {
-  float: right;
-  padding-top: 1vh;
-  padding-right: 1vw;
-  color: #828282;
-}
-.overlayBorrowClose:hover {
-  color: white;
-}
-.overlayBorrowClose:active {
-  color: #0E639C;
-}
-#overlayBorrowButton {
-  background-color: #0E639C;
-  color: white;
-  text-align: center;
-}
-#bookListHandBackButton {
-  grid-column-start: 1;
-  grid-column-end: 2;
-  grid-row-start: 4;
-  grid-row-end: 5;
-  height: 6vh;
-  background-color: #0E639C;
-  color: white;
-}
+@import "../assets/book.module.css";
 </style>
