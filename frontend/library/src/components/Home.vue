3f93ccccd6ee32e02b6c9eadca082c6f52458516
Marcey1611 <marcel.eichelberger@t-online.de>
2023-06-25 18:18:26 +0200

    Home auswählen eines Buches möglich + ausleihen ...

--- frontend/library/src/components/Home.vue
+++ frontend/library/src/components/Home.vue
@@ -1,87 +1,154 @@
 <template>
-    <!--Test-->
-        <h1 id="title">Top 10 Bücher</h1>
-        <v-sheet
-            class="mx-auto"
-            width="87vw"
-            height="50vh"
-            color="#1e1e1e"
-        >
-            <v-slide-group
-                v-model="model"
-                class="pa-4"
-                center-active
-            >
-                <v-slide-group-item
-                    v-for="book in this.top10books"
-                    :key="book"
-                    v-slot="{ isSelected, toggle }"
-                >
-                    <v-card
-                        :color="isSelected ? '#0E639C' : '#828282'"
-                        class="ma-4"
-                        height="40vh"
-                        width="15vw"
-                        @click="toggle"
-                    >
+    <h1 id="title">Top 10 Bücher</h1>
+    <div id="top10Books">
+        <v-sheet class="mx-auto" width="87vw" height="50vh" color="#2d2d2d">
+            <v-slide-group v-model="model" class="pa-4" center-active>
+                <v-slide-group-item v-for="book in this.top10books" :key="book" v-slot="{ isSelected, toggle }">
+                    <v-card :color="isSelected ? '#0E639C' : '#828282'" class="ma-4" max-height="100%" width="100%"
+                        @click="toggle">
                         <div class="d-flex fill-height align-center justify-center">
                             <v-scale-transition>
-                                <img id="cover" :src="book.img" alt="Werbung">
-                                <h1>1</h1>
+                                
+                                <img id="cover" :src="book.img" alt="Werbung" @click="showMoreInfos(book)">
+                                
                             </v-scale-transition>
                         </div>
                     </v-card>
                 </v-slide-group-item>
             </v-slide-group>
         </v-sheet>
+    </div>
+    <br>
+
+    <div v-if="showMoreInfosBool" id="top10BooksSelectedBooks">
+        <Book :book="this.selectedBook"  @borrow="borrow"
+            @handback="handback" />
+    </div>
+
+    <br>
+    <div id="generalInfos">
+        <h1>Wie es funktioniert!?</h1>
+        <br>
+        <p>Hi, du bist wahrscheinlich neu hier und willt wissen wie "The OnLibrary" funktioniert.</p>
+        <br>
+        <p>Das Grundprinzip hinter dieser Website ist eine einfach Bücherei nur moderner und innovativer. Eigentlich ist es
+            ganz einfach:</p>
+        <br>
+        <p>Jeder der möchte kann bei uns, über die Add-Seite, seine Bücher hochladen, um diese anderen ausleihen zu können.
+            Auf der Books-Seite siehst du alle Bücher der Community, über die Filter-und Such-Bar kannst du die Liste nach
+            belieben filtern
+            und durchsuchen. Wenn du ein Buch gefunden hast welches du bei einem anderen ausleihen möchtest kannst du
+            unterhalb der Buchinfos,
+            falls das Buch verfügbar ist, dein Name angeben und dich bei seinem Besitzer melden. Viel Spaß!</p>
+
+    </div>
+    <br>
 </template>
 
 <script>
-    import axios from 'axios';
-    export default {
-        data: () => ({
-            model: null,
-            topanzeige: [],
-            books: [],
-            top10books: [],
-        }),
-        methods: {
-            async getBookCover(){
-                const response = await axios.get('http://localhost:8080/books');
-                this.books = response.data;
-                let length = this.books.length;
-                let pos = 0;
-                let len = 10;
-                if(length < 10){
-                    len = length;
-                }
-                for(let i=0; i<len; i++){
-                    this.topanzeige[i] = "Top"
-                    for(let n=0; n<length; n++){
-                        if(this.books[n].borrowCount > this.books[pos].borrowCount){
-                            pos = n;
-                        }
-                    }
-                    this.top10books.push(this.books[pos]);
-                    this.books.splice(pos, 1);
-                    pos = 0;
-                    length--;
-                }
-            }
+import axios from 'axios';
+import Book from './Book.vue';
+
+export default {
+    components: {
+        Book
+    },
+    data: () => ({
+        selectedBook: null,
+        showMoreInfosBool: false,
+        model: null,
+        topanzeige: [],
+        books: [],
+        top10books: [],
+    }),
+    methods: {
+        borrow: function (e) {
+      axios
+        .put("http://localhost:8080/books/" + e.id, {
+          firstName: e.firstName,
+          lastName: e.lastName,
+          available: false,
+        })
+        .then(response => {
+            window.location.reload();
+        });
+    },
+    handback: function (e) {
+      axios
+        .put("http://localhost:8080/books/" + e.id, {
+          firstName: '',
+          lastName: '',
+          available: true
+        })
+        .then(response => {
+            window.location.reload();
+        });
+    },
+        showMoreInfos(book) {
+            this.showMoreInfosBool = true;
+            this.selectedBook = book;
+
         },
-        mounted() {
-            this.getBookCover()
+
+        async getBookCover() {
+            const response = await axios.get('http://localhost:8080/books');
+            this.books = response.data;
+            let length = this.books.length;
+            let pos = 0;
+            let len = 10;
+            if (length < 10) {
+                len = length;
+            }
+            for (let i = 0; i < len; i++) {
+                this.topanzeige[i] = "Top"
+                for (let n = 0; n < length; n++) {
+                    if (this.books[n].borrowCount > this.books[pos].borrowCount) {
+                        pos = n;
+                    }
+                }
+                this.top10books.push(this.books[pos]);
+                this.books.splice(pos, 1);
+                pos = 0;
+                length--;
+            }
         }
+    },
+    mounted() {
+        this.getBookCover()
     }
+}
 </script>
 
 <style>
-    #title{
-        margin-left: 10vw;
-        color: #828282;
-    }
-    #cover{
-        width: 20vw;
-        height: 40vh;
-    }
+#title {
+    color: #828282;
+}
+
+#cover {
+    width: 200px;
+    height: 350px;
+}
+
+#top10Books {
+    background-color: #2d2d2d;
+    width: 95%;
+    border-radius: 1vh 1vh 1vh 1vh;
+}
+
+#top10BooksSelectedBooks {
+    width: 95%;
+}
+
+#generalInfos {
+    background-color: #2d2d2d;
+    width: 95%;
+    border-radius: 1vh 1vh 1vh 1vh;
+    color: #828282;
+    padding-left: 10vw;
+    padding-right: 10vw;
+    padding-top: 1vh;
+    padding-bottom: 3vh;
+    text-align: center;
+    margin-bottom: 100px;
+}
 </style>
