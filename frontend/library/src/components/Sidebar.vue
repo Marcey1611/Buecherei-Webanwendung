c266ff31e2506b7afe51e864c300de2a47abf7ad
MikeLach22 <mikelachmuth@gmail.com>
2023-06-25 11:47:34 +0200

    Revert the Revert

--- frontend/library/src/components/Sidebar.vue
+++ frontend/library/src/components/Sidebar.vue
@@ -1,5 +1,4 @@
 <template>
-
   <v-card>
     <v-layout>
       <v-navigation-drawer
@@ -31,7 +30,7 @@
 
         <v-divider></v-divider>
 
-        <v-list density="compact" nav>
+        <v-list density="compact" nav id="navBar">
             <v-list-item 
                 class="text"
                 prepend-icon="mdi-home-city" 
@@ -43,32 +42,40 @@
                 class="text"
                 prepend-icon="mdi-book" 
                 title="Books" 
-                value="account"
+                value="books"
                 to="/books"
             />
             <v-list-item 
                 class="text"
                 prepend-icon="mdi-plus" 
                 title="Add book" 
-                value="users"
+                value="add"
                 to="/add"
             />
+            <v-divider></v-divider>
+            
         </v-list>
+        <div id="contact">
+          <v-list-item title="Contact us:" nav/>
+          <v-list-item title="Mail: wahreailer@gmail.com" nav/>
+          <v-list-item title="Phone: 1234" nav/>
+        </div>
+        
       </v-navigation-drawer>
       <!--<v-main style="height: 250px"></v-main>-->
     </v-layout>
+    
   </v-card>
-
 </template>
 
 <script>
     import { RouterLink, RouterView } from 'vue-router'
-
     export default {
         data () {
             return {
                 drawer: true,
                 rail: true,
+                activeItem: 'home',
             }
         },
     }
@@ -79,8 +86,14 @@
         background-color: #3c3c3c;
         color: #a6a6a6;
     }
-    
     .text{
         color: #a6a6a6; 
+    }
+    #contact{
+      position: absolute;
+      bottom: 0;
+    }
+    #navBar{
+      position: relative;
     }
 </style>
